56b4d8d918970c3d557bc514a487bcadc35d885d
Dima Legeza <dmytro.legeza@powerhouse.nl>
2017-03-06 11:54:38 +0100

    finally working generator

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,14 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages = "org.hibernate.liquibase")
 @EnableAutoConfiguration
+@ComponentScan(basePackages = "org.hibernate.liquibase")
+@EnableJpaRepositories(basePackages = "org.hibernate.liquibase.repository")
+@EntityScan(basePackages = "org.hibernate.liquibase.entities")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
