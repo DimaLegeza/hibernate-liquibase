6be38a83dce254688bec425a6167d19365804fe3
Alex <alex.bowkun@gmail.com>
2017-03-08 22:56:41 +0100

    revision entity extended

--- src/main/java/org/hibernate/liquibase/entities/Person.java
+++ src/main/java/org/hibernate/liquibase/entities/Person.java
@@ -3,6 +3,7 @@ package org.hibernate.liquibase.entities;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Audited(withModifiedFlag = true)
 public class Person {
 
     @Id
