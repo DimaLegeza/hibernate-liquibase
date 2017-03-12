e82f8c6a727337c26550222c843b0ab778e93825
Dima Legeza <dmytro.legeza@powerhouse.nl>
2017-03-12 14:57:38 +0100

    project config

--- src/main/java/org/homemade/testproject/Application.java
+++ src/main/java/org/homemade/testproject/Application.java
@@ -7,11 +7,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "org.homemade.testproject")
 @EnableJpaRepositories(basePackages = "org.homemade.testproject.repository")
 @EnableWebMvc
+@EnableSwagger2
 public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
