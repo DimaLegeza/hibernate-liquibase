b547adeba6d52caefdd2bf424a4a7830123dd0b7
Dima Legeza <dmytro.legeza@powerhouse.nl>
2017-03-12 20:34:40 +0100

    added second entity with relation one-to-many and many-to-one back

--- src/main/java/org/homemade/testproject/controller/ElementController.java
+++ src/main/java/org/homemade/testproject/controller/ElementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "element-variable")
+@RequestMapping(value = "element")
 public class ElementController {
 
 	@Autowired
