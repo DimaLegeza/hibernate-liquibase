e53bfc88000c677b146b23dbfc8eb059bdc03635
Dima Legeza <dmytro.legeza@powerhouse.nl>
2017-03-12 14:53:13 +0100

    project config

--- src/main/java/org/homemade/testproject/repository/ElementVariableRepository.java
+++ src/main/java/org/homemade/testproject/repository/ElementVariableRepository.java
@@ -3,7 +3,9 @@ package org.homemade.testproject.repository;
 import org.homemade.testproject.entites.product.ElementVariable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ElementVariableRepository extends
 	JpaRepository<ElementVariable, Long>, JpaSpecificationExecutor<ElementVariable> {
 }
