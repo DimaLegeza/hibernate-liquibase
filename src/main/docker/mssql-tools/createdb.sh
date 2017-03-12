47403ddcdbb2bcf64b7c74b05899f014593d49f4
Dima Legeza <dmytro.legeza@powerhouse.nl>
2017-03-12 14:32:50 +0100

    migrated project to Gradle with Docker

--- src/main/docker/mssql-tools/createdb.sh
+++ src/main/docker/mssql-tools/createdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/mssql-tools/bin/sqlcmd -S database -U SA -P 'FakePass54321&' -Q "CREATE DATABASE hibernate-liquibase"
+/opt/mssql-tools/bin/sqlcmd -S database -U SA -P 'FakePass54321&' -Q "CREATE DATABASE hibernateliquibase"
--- src/main/java/org/homemade/testproject/Application.java
+++ src/main/java/org/homemade/testproject/Application.java
@@ -1,9 +1,17 @@
 package org.homemade.testproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "org.homemade.testproject")
+@EnableJpaRepositories(basePackages = "org.homemade.testproject.repository")
+@EnableWebMvc
 public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
