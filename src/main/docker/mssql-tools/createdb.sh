66da1ea5cb9fb3fdceaa33afeeb17713941e2b02
Dima Legeza <dmytro.legeza@powerhouse.nl>
2017-03-12 14:37:43 +0100

    project config

--- src/main/docker/mssql-tools/createdb.sh
+++ src/main/docker/mssql-tools/createdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/mssql-tools/bin/sqlcmd -S database -U SA -P 'FakePass54321&' -Q "CREATE DATABASE hibernateliquibase"
+/opt/mssql-tools/bin/sqlcmd -S database -U SA -P 'FakePass51&' -Q "CREATE DATABASE hibernateliquibase"
